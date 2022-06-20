3067076e23e27ce3e242a8ededb5945f35a2f701
githubSadhamHussainA <githubsadhamhussain.a7@gmail.com>
2022-06-20 15:24:20 +0530

    You want to become a world best Developer

---
+++ src/main/java/GitHubStoring/githubstoringProject/App.java
@@ -0,0 +1,13 @@
+package GitHubStoring.githubstoringProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
